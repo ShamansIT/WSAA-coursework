f05297b0c42b86abc48007e607a2fdd376fc409e
Serhii Spitsyn <serhii.spitsyn.ie@gmail.com>
2024-02-04 13:16:12 +0000

    Assignment Current Weather Finish

--- assignments/Assignment_CurrentWeather/currentweather.py
+++ assignments/Assignment_CurrentWeather/currentweather.py
@@ -1,3 +1,24 @@
 
+import requests
 
-url = "https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m"
+latitude = 51.898
+longitude = -8.4706
+
+# Define the URL for the JSON API
+url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,wind_direction_10m"
+
+# Send a GET request to the API
+response = requests.get(url)
+
+# Parse the JSON data
+data = response.json()
+
+# check connection
+# print(data)
+
+# Extract and print the current data
+current_temperature = data["current"]["temperature_2m"]
+current_wind_direction = data["current"]["wind_direction_10m"]
+
+print("Current temperature: ", current_temperature, chr(176)+"C")
+print("Current wind direction: ", current_wind_direction)
---
+++ assignments/Assignment_CurrentWeather/readme.txt
@@ -0,0 +1,7 @@
+Program show current temperature and wind dirrection in location. 
+
+For change location change latitude and longitude parameters.
+
+Use api data: https://open-meteo.com/en/docs/
+
+Solve problem with Celsium write resource: https://java2blog.com/print-degree-symbol-python/
