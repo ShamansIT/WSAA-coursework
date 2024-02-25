dff2a86a1342132d2183ec21c6451175b9327c59
Serhii Spitsyn <serhii.spitsyn.ie@gmail.com>
2024-02-25 20:19:00 +0000

    Correct token

--- labs/Lab5/Lab_05.02_Chalange1.py
+++ labs/Lab5/Lab_05.02_Chalange1.py
@@ -3,11 +3,12 @@ import json
 
 # Assuming config.py contains your GitHub API key
 from config import apiKey
+from config import token
 
 url = 'https://api.github.com/repos/ShamansIT/aprivateone'
 
 # Make the request with authentication
-response = requests.get(url, headers={"Authorization": f"token {apiKey}"})
+response = requests.get(url, headers={"Authorization": f"{token} {apiKey}"})
 repoJSON = response.json()
 
 # Print detailed information about the repository
